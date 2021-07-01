b4d0cd7dafb7e7cbc6ba50a195d348a1a38d7928
Kaua Marangoni <kauamarangonitj@gmail.com>
2021-07-01 17:19:45 -0300

    Added user not found screen

---
+++ main.js
@@ -0,0 +1,14 @@
+const closePoupUp = document.querySelector(".close-poup-up")
+const divContainer = document.querySelector(".container-poup-up")
+const openPoupUp = document.querySelector("button.button-login")
+const divPoupUp = document.querySelector(".div-poup-up")
+
+openPoupUp.onclick = () => {
+    divContainer.style.display = "flex"
+    divPoupUp.style.display = "inline"
+}
+
+closePoupUp.onclick = () => {
+    divContainer.style.display = "none"
+    divPoupUp.style.display = "none"
+}
